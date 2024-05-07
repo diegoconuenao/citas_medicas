0626642b2624a0640f519eefd722b58d4030abf0
diegoconuenao <diego4124@gmail.com>
2024-05-07 19:31:56 -0400

    primer commit

---
+++ app/src/main/java/com/example/citas_medicas/LoginScreen.kt
@@ -0,0 +1,95 @@
+package com.example.citas_medicas
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen(){
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.logomedico), contentDescription = "Login Imagen",
+            modifier = Modifier.size(200.dp))
+
+        Text(text = "Bienvenido a Citas Médicas", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(text = "Ingresa a tu cuenta")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+
+        }, label = {
+            Text(text = "Ingrese Email")
+        })
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = {
+            Text(text = "Contraseña")
+        }, visualTransformation = PasswordVisualTransformation())
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {  }) {
+            Text(text = "Acceder")
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = "¿Olvidó Contraseña?", modifier = Modifier.clickable {
+        })
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(text = "O ingrese con")
+
+        Image(painter = painterResource(id = R.drawable.gmail),
+            contentDescription = "Gmail",
+            modifier = Modifier
+                .size(60.dp)
+                .clickable {
+                    //Click de Gmail
+                }
+        )
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/citas_medicas/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.citas_medicas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.citas_medicas.ui.theme.Citas_medicasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            //MyComponent()
+            LoginScreen()
+        }
+    }
+}
+/*
+@Composable
+fun MyComponent(){
+    Row (modifier = Modifier.padding(8.dp)){
+        MyImage()
+        MyTexts()
+    }
+}
+@Composable
+fun MyImage(){
+    Image(
+        painterResource(R.drawable.ic_launcher_foreground),
+        "Mi imagen de prueba",
+        modifier = Modifier.clip(CircleShape).background(Color.Gray)
+    )
+}
+
+@Composable
+fun MyTexts(){
+    Column(modifier = Modifier.padding(start = 8.dp)) {
+        MyText("Hola Jetpack Compose!")
+        Spacer(modifier = Modifier.height(16.dp))
+        MyText("Preparado?")
+    }
+}
+
+@Composable
+fun MyText(text: String){
+    Text(text)
+}
+
+@Preview
+@Composable
+fun PreviewComponent(){
+    MyComponent()
+}
+
+ */
+
+@Preview
+@Composable
+fun PreviewComponent(){
+    LoginScreen()
+}
